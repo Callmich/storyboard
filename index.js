2c5f1b295983c8c8c4240f3d88555684ea0d786d
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-20 18:51:51 -0600

    set up more pieces of the router and init the knexfile as well as brought in the first migration. Ready to set up the migration for the tables.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 5959;
+const PORT = process.env.PORT || 4242;
 
 server.listen(PORT, ()=>{
     console.log(`\n **** Listening on port ${PORT}****\n`)

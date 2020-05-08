293f5af4732bf76f13e508c1648cc3fe9a78bc12
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-07 22:03:05 -0600

    added a few Cruds onto the Characters router

--- characters/characters-router.js
+++ characters/characters-router.js
@@ -6,4 +6,39 @@ const router = express.Router();
 
 //CRUD ACTIONS go here and will start with /api/characters
 
+router.get('/', (req, res) => {
+    Characters.findCharacters()
+      .then(characters => {
+          res.status(200).json(characters)
+      })
+      .catch(error =>{
+          res.status(500).json({ message: "Failed to get characters from server"})
+      })
+})
+
+router.get('/:id', (req, res) => {
+    const {id} =req.params
+
+    Characters.findCharacterById(id)
+      .then(char => {
+        res.status(200).json(char)
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({ message: "Failed to get character from server"})
+      })
+})
+
+router.post("/", (req, res) => {
+    const charData = req.body;
+
+    Characters.addCharacter(charData)
+      .then(newChar => {
+          res.status(201).json(newChar)
+      })
+      .catch(error => {
+          res.status(500).json({message: `Failed to Create a new Project ${error.message}`})
+      })
+})
+
 module.exports = router;

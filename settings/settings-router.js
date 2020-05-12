12eae6f36b07ab82d56aaf79076fc7cadc0e2300
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-12 08:29:54 -0600

    adding CRUD actions into the settings server

--- settings/settings-router.js
+++ settings/settings-router.js
@@ -6,4 +6,14 @@ const router = express.Router();
 
 //CRUD ACTIONS go here and will start with /api/settings
 
+router.get('/', (req, res) => {
+    Settings.findSettings()
+      .then(settings => {
+          res.status(200).json(settings)
+      })
+      .catch(error => {
+          res.status(500).json({ message: "Failed to get settings from server"})
+      })
+})
+
 module.exports = router;

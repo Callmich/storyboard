52540f070c986cc2a31ebbbfc084c9ab968ab169
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-15 10:42:17 -0600

    added put CRUD functionality into the Settings Router

--- settings/settings-router.js
+++ settings/settings-router.js
@@ -36,7 +36,31 @@ router.post('/', (req, res) => {
           res.status(201).json(newSet)
       })
       .catch(error => {
-          res.status(500).json({message: `Failed to Create a new Project ${error.message})
+          res.status(500).json({message: `Failed to Create a new Project ${error.message}`)
+      })
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    Settings.findSettingById(id)
+      .then(set => {
+          if(set){
+            Settings.updateSetting(id, changes)
+              .then(updatedSet => {
+                  res.status(200).json(updatedSet)
+              })
+              .catch(error => {
+                  console.log(error)
+                  res.status(404).json({message: "error while updating Char - please confirm the correct info is being sent."})
+              })
+          } else{
+            res.status(404).json({ message: `Could not find setting with id ${id}`});
+          }
+      })
+      .catch(error => {
+        res.status(500).json({error: `Failed to update setting ${error.message}`})
       })
 })
 

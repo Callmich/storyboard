18ea717b38f2d0c164f65ba70c66c36b07091d1c
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-17 18:44:02 -0600

    adding begining of the scenes functions for scenes-model.js

--- scenes/scenes-model.js
+++ scenes/scenes-model.js
@@ -1,1 +1,25 @@
+const dB = require('../data/dbConfig.js');
 
+module.exports = {
+    findScenes,
+    findSceneById,
+    findScenesByStoryId,
+    addScene,
+    updateScene,
+    removeScene
+};
+
+function findScenes(){
+    return dB('scenes')
+};
+
+function findSceneById(id){
+    return dB('scenes')
+    .where({ id })
+    .first()
+};
+
+function findScenesByStoryId(story_id){
+    return dB('scenes')
+    .where({ story_id })
+};

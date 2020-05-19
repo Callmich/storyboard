1bf6927fa0ccf91e4ce837dbde06919854cd6362
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-19 07:41:47 -0600

    addScene added to scenes-model

--- scenes/scenes-model.js
+++ scenes/scenes-model.js
@@ -23,3 +23,14 @@ function findScenesByStoryId(story_id){
     return dB('scenes')
     .where({ story_id })
 };
+
+function addScene(scene){
+    return dB('scenes')
+    .insert(scene, 'id')
+    .then(([id]) => {
+        return findSceneById(id)
+    })
+    .catch(error => {
+        console.log('Error on add scene', error)
+    })
+};

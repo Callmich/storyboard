d6bb512f2ec1303afa65811784b9060c25ded12b
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-28 08:03:49 -0600

    adding in functions specific for joins in story_character-model

--- routers/middleTableRouters/story_characters-Router.js
+++ routers/middleTableRouters/story_characters-Router.js
@@ -6,7 +6,7 @@ const StoryCharacter = require('../../shared-models/middleTable-Models/story_cha
 const router = express.Router();
 
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
     const storyCharData = req.body;
 
     SharedFunc.add('story_characters', storyCharData)
@@ -15,6 +15,45 @@ router.post("/", (req, res) => {
       })
       .catch(error => {
           res.status(500).json({message: `Failed to create link for Story and Character ${error}`})
+      })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  SharedFunc.findById('story_characters', id)
+    .then(storyChar => {
+      if(storyChar){
+        SharedFunc.update('story_characters', id, changes)
+        .then(updatedItem => {
+            res.status(200).json(updatedItem)
+          })
+          .catch(error => {
+            res.status(500).json({message: `Error updating the story_Characters connection with server Error: ${error}`})
+          })
+      }else{
+        res.status(404).json({ message: `Could not find connection between character and Story`})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({error: `Failed to update story_characters connection ${error}`})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    SharedFunc.remove('story_characters', id)
+      .then(deleted => {
+        if(deleted){
+          res.status(200).json({removed: deleted})
+        }else{
+          res.status(404).json({error: `Could not find story Characters with id ${id}`})
+        }
+      })
+      .catch(error => {
+          res.status(500).json({error: `Server unable to delete story_character Error: ${error}`})
       })
 })
 
--- shared-models/middleTable-Models/story_characters-model.js
+++ shared-models/middleTable-Models/story_characters-model.js
@@ -5,6 +5,8 @@ module.exports = {
 }
 
 function find(){
-  return dB('stories as s')
-    .leftJoin('characters as c', '')
+  return dB('story_characters as sc')
+    .join('characters as c', 'sc.character_id', 'c.id')
+    .join('stories as s', 'sc.story_id', 's.id')
+    .select('sc.id as story_character_id','sc.pov_changes', 's.name as story_name', 's.id as story_id', 'c.name as character_name', 'c.id as character_id')
 }

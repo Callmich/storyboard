06dc63830ef6cb38acc8fac409326c3e6a1c9307
Michael Callahan <CallahanMichaelE@gmail.com>
2020-05-30 23:52:44 -0600

    adding in additional functions for story character

--- shared-models/middleTable-Models/story_characters-model.js
+++ shared-models/middleTable-Models/story_characters-model.js
@@ -3,7 +3,8 @@ const dB = require('../../data/dbConfig.js')
 module.exports = {
     find,
     findById,
-    findByProjectId
+    findByProjectId,
+    test
 }
 
 function find(){
@@ -28,4 +29,4 @@ return dB('story_characters as sc')
 .join('stories as s', 'sc.story_id', 's.id')
 .where({ project_id })
 .select('sc.id as story_character_id','sc.pov_changes', 's.name as story_name', 's.id as story_id', 's.project_id as project_id', 'c.name as character_name', 'c.id as character_id')
-}
+} //check in postman

1a8bff7b4584a6a1eb13861b5597f10e666db04e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-06-01 13:37:46 -0600

    updating story_character-model.js with the new column names from the migration

--- shared-models/middleTable-Models/story_characters-model.js
+++ shared-models/middleTable-Models/story_characters-model.js
@@ -3,15 +3,14 @@ const dB = require('../../data/dbConfig.js')
 module.exports = {
     find,
     findById,
-    findByProjectId,
-    test
+    findByProjectId
 }
 
 function find(){
   return dB('story_characters as sc')
-    .join('characters as c', 'sc.character_id', 'c.id')
-    .join('stories as s', 'sc.story_id', 's.id')
-    .select('sc.id as story_character_id','sc.pov_changes', 's.name as story_name', 's.id as story_id', 's.project_id as project_id', 'c.name as character_name', 'c.id as character_id')
+    .join('characters as c', 'sc.character_id', 'c.character_id')
+    .join('stories as s', 'sc.story_id', 's.story_id')
+    .select('sc.story_character_id','sc.story_character_pov_changes', 's.story_name', 's.story_id', 's.project_id', 'c.character_name', 'c.character_id')
 }
 
 function findById(id){

dae5f81d0a5d363d4a7b496fb27de0b4865634c6
Michael Callahan <CallahanMichaelE@gmail.com>
2020-06-02 08:26:00 -0600

    adjusted story_character-model to new column names for tables

--- shared-models/middleTable-Models/story_characters-model.js
+++ shared-models/middleTable-Models/story_characters-model.js
@@ -17,15 +17,15 @@ function findById(id){
   return dB('story_characters as sc')
   .where({ id })
   .first()
-  .join('characters as c', 'sc.character_id', 'c.id')
-  .join('stories as s', 'sc.story_id', 's.id')
-  .select('sc.id as story_character_id','sc.pov_changes', 's.name as story_name', 's.id as story_id', 's.project_id as project_id', 'c.name as character_name', 'c.id as character_id')
+  .join('characters as c', 'sc.character_id', 'c.character_id')
+  .join('stories as s', 'sc.story_id', 's.story_id')
+  .select('sc.story_character_id','sc.story_character_pov_changes', 's.story_name', 's.story_id', 's.project_id', 'c.character_name', 'c.character_id')
 }
 
 function findByProjectId(project_id){
 return dB('story_characters as sc')
-.join('characters as c', 'sc.character_id', 'c.id')
-.join('stories as s', 'sc.story_id', 's.id')
+.join('characters as c', 'sc.character_id', 'c.character_id')
+.join('stories as s', 'sc.story_id', 's.story_id')
 .where({ project_id })
-.select('sc.id as story_character_id','sc.pov_changes', 's.name as story_name', 's.id as story_id', 's.project_id as project_id', 'c.name as character_name', 'c.id as character_id')
+.select('sc.story_character_id','sc.story_character_pov_changes', 's.story_name', 's.story_id', 's.project_id', 'c.character_name', 'c.character_id')
 } //check in postman

e848ddd415b5a7103992e012feb5135119315b12
Michael Callahan <CallahanMichaelE@gmail.com>
2020-06-07 10:59:08 -0600

    updated findByProjectId in character_settings-model - it now is pulling correctly

--- shared-models/middleTable-Models/character_settings-model.js
+++ shared-models/middleTable-Models/character_settings-model.js
@@ -22,10 +22,12 @@ function findById(character_setting_id){
     .select('cs.character_setting_id', 'c.character_name', 's.setting_name', 'c.project_id')
 }
 
+// c.project_id renamed to pro_id to avoid ambiguous tables as both c & s have project_id
 function findByProjectId(project_id){
+  let pro_id = project_id
   return dB('character_settings as cs')
     .join('characters as c', 'cs.character_id', 'c.character_id')
     .join('settings as s', 'cs.setting_id', 's.setting_id')
-    .where( {project_id} )
-    .select('cs.character_setting_id', 'c.character_name', 's.setting_name', 'c.project_id')
+    .select('cs.character_setting_id', 'c.character_name', 's.setting_name', 'c.project_id as pro_id')
+    .where( {pro_id} )
 }

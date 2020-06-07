e2e125cd0e958befe4f150b4ec5f12315cdb6486
Michael Callahan <CallahanMichaelE@gmail.com>
2020-06-07 16:49:12 -0600

    added comments and findBtCharacterId to scene_characters-model

--- shared-models/middleTable-Models/scene_characters-model.js
+++ shared-models/middleTable-Models/scene_characters-model.js
@@ -3,6 +3,7 @@ const dB = require('../../data/dbConfig.js')
 module.exports = {
     find,
     findById,
+    findByCharacterId,
     findByProjectId
 }
 
@@ -22,11 +23,22 @@ function findById(scene_characters_id){
     .select('sc.scene_character_id', 'c.character_name', 's.scene_name', 'sc.scene_character_pov_changes', 'c.project_id')
 }
 
+// c.project_id renamed to avoid ambiguous column
 function findByProjectId(project_id){
   let pro_id = project_id
   return dB('scene_characters as sc')
     .join('scenes as s', 'sc.scene_id', 's.scene_id')
     .join('characters as c', 'sc.character_id', 'c.character_id')
-    .select('sc.scene_character_id', 'c.character_name', 's.scene_name', 'sc.scene_character_pov_changes', 'c.project_id')
+    .select('sc.scene_character_id', 'c.character_name', 's.scene_name', 'sc.scene_character_pov_changes', 'c.project_id as pro_id')
     .where({ pro_id })
+}
+
+// c.character_id renamed to avoid ambiguous column
+function findByCharacterId(character_id){
+  let char_id = character_id
+  return dB('scene_characters as sc')
+    .join('scenes as s', 'sc.scene_id', 's.scene_id')
+    .join('characters as c', 'sc.character_id', 'c.character_id')
+    .select('sc.scene_character_id', 'c.character_name', 's.scene_name', 'sc.scene_character_pov_changes', 'c.chatacter_id as char_id')
+    .where({ char_id })
 }

13c286097a5d8a8c29f73637babe5cada60ee622
Michael Callahan <CallahanMichaelE@gmail.com>
2020-06-17 08:27:02 -0600

    trying to adjust testing but still showing an empty array

--- tests/project.test.js
+++ tests/project.test.js
@@ -2,18 +2,31 @@ const supertest = require('supertest');
 const server = require('../server');
 
 
-jest.mock("../shared-models/shared-models");
-const Projects = require('../shared-models/shared-models.js');
+// jest.mock("../shared-models/shared-models.js");
+// const Projects = require('../shared-models/shared-models.js');
+
+// describe('projects endpoints', () => {
+//   describe('GET/', () => {
+//     it('should return mesage with 0', async () => {
+//       Projects.findAll = (dataBase) => {
+//         dataBase = 'projects'
+//         return ['test']}
+//       const res = await supertest(server).get('/api/projects/')
+
+//     //   expect(200)
+//       expect(res.body).toEqual("message: Nope!")
+//     });
+//   })
+// })
+
+jest.mock("../shared-models/middleTable-Models/character_settings-model");
+const CS = require('../shared-models/middleTable-Models/character_settings-model');
 
 describe('projects endpoints', () => {
-  
   describe('GET/', () => {
     it('should return mesage with 0', async () => {
-      
-      Projects.findAll = (dataBase) => {
-        dataBase = 'projects'
-        return ['test']}
-      const res = await supertest(server).get('/api/projects/')
+      // CS.find = () => {}
+      const res = await supertest(server).get('/api/characterSettings/1')
 
     //   expect(200)
       expect(res.body).toEqual("message: Nope!")
